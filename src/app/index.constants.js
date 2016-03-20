79dcd407e6dcc1be511491a8293e38c540745f80
Jordan Skole <jordan@usebenchmark.com>
2016-03-20 19:59:57 -0400

    yo!

---
+++ src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('cmAdmin')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
