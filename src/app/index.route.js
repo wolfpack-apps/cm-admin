27f19bbfba7ae862f89e237eeaf7007621b2c9bf
Jordan Skole <jordan@usebenchmark.com>
2016-03-23 00:57:42 -0400

    account abstract view

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -13,6 +13,28 @@
         templateUrl: 'app/account/tmpl.login.html',
         controller: 'LoginController',
         controllerAs: 'LoginCtrl'
+      })
+      .state('li', {
+        resolve: {
+          // controller will not be loaded until $requireAuth resolves
+          // Auth refers to our $firebaseAuth wrapper in the example above
+          "CurrentAuth": ["Auth", function(Auth) {
+            // $requireAuth returns a promise so the resolve waits for it to complete
+            return Auth.$requireAuth();
+          }],
+          "API_KEY": ["FIREBASE_URL", "$firebaseObject", function (FIREBASE_URL, $firebaseObject) {
+            var itemsRef = new Firebase(FIREBASE_URL + "/edmunds_key");
+            var obj = $firebaseObject(itemsRef);
+
+            return obj.$loaded();
+
+          }]
+        },
+        url: '/+',
+        templateUrl: 'app/account/tmpl.li.html',
+        controller: 'AccountController',
+        controllerAs: 'AccountCtrl',
+        abstract: true
       });
 
     $urlRouterProvider.otherwise('/');
--- src/app/index.run.js
+++ src/app/index.run.js
@@ -6,7 +6,20 @@
     .run(runBlock);
 
   /** @ngInject */
-  function runBlock($log) {
+  function runBlock($log, $rootScope, $state) {
+
+    $rootScope.$on('$stateChangeError', function (event, toState, toParams, fromState, fromParams, error) {
+      // throw the error
+      $log.error(error);
+
+      // We can catch the error thrown when the $requireAuth promise is rejected
+      // and redirect the user back to the home page
+      if (error === "AUTH_REQUIRED") {
+        $state.go("login");
+      }
+    });
+
+    $rootScope.$state = $state;
 
     $log.debug('runBlock end');
   }
