e3d232178a4163bbecda26b29b39328a2b950ee8
Jordan Skole <jordan@usebenchmark.com>
2016-04-06 23:23:14 -0400

    errors prevent next steps w/o validation

--- src/app/register/ctrl.club.js
+++ src/app/register/ctrl.club.js
@@ -10,6 +10,27 @@
 
     var vm = this;
 
+    vm.register = function () {
+      $log.log('Attempting to register a new user');
+      $log.log(vm.user);
+      Auth.$createUser({
+        email: vm.user.email,
+        password: vm.user.password
+      })
+      .then(function (userData) {
+        // create an account model with the user data
+        $log.log("User created with uid: " + userData.uid);
+      })
+      .then(function (userData) {
+        // send the user to the next stage in the form.
+        $state.go('li.payments')
+      })
+      .catch(function(error) {
+        $log.warn(error)
+        vm.loginError = error;
+      });
+    }
+
   }
 
 })();
