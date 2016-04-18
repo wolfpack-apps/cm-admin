279e6d23fba2133221467dd736a0bd4a73b5241a
Jordan Skole <jordan@usebenchmark.com>
2016-04-18 10:06:02 -0400

    comments so we dont keep creating managers

--- src/app/account/ctrl.detail.js
+++ src/app/account/ctrl.detail.js
@@ -16,22 +16,22 @@
     /*
      * Functions
      */
-    manager.$loaded()
-      .then(function () {
-        $log.log('loaded');
-        manager.first_name = 'Jordan';
-        manager.last_name = 'Skole';
-        manager.email = 'jordan@skole.us';
-        manager.cell_phone = '2488817460';
-        manager.work_phone = '3135551234';
-        manager.birthday = '1985-07-16T19 =20+01 =00';
-        manager.street = '169 Saxon Ct';
-        manager.street2 = 'Ste 200';
-        manager.city = 'Rochester Hills';
-        manager.state = 'MI';
-
-        manager.$save();
-      })
+    // manager.$loaded()
+    //   .then(function () {
+    //     $log.log('loaded');
+    //     manager.first_name = 'Jordan';
+    //     manager.last_name = 'Skole';
+    //     manager.email = 'jordan@skole.us';
+    //     manager.cell_phone = '2488817460';
+    //     manager.work_phone = '3135551234';
+    //     manager.birthday = '1985-07-16T19 =20+01 =00';
+    //     manager.street = '169 Saxon Ct';
+    //     manager.street2 = 'Ste 200';
+    //     manager.city = 'Rochester Hills';
+    //     manager.state = 'MI';
+    //
+    //     manager.$save();
+    //   })
 
 
   }
