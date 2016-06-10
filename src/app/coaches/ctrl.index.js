973a414850125ba18941a733c8509cd661f96eb0
Jordan Skole <jordan@usebenchmark.com>
2016-06-10 00:02:17 -0400

    modal is wired up well

--- src/app/coaches/ctrl.index.js
+++ src/app/coaches/ctrl.index.js
@@ -11,17 +11,18 @@
     var vm = this;
     vm.data = $state.current.data;
 
+    // this needs to be turned into a service
     vm.inviteCoach = function(ev) {
       // Appending dialog to document.body to cover sidenav in docs app
       var confirm = $mdDialog.prompt()
-        .title('What would you name your dog?')
-        .textContent('Bowser is a common name.')
-        // .placeholder('Dog name')
-        .ariaLabel('Dog name')
+        .title('Invite a coach')
+        .textContent('Enter the coach\'s email address, and we will send them an invite')
+        .placeholder('jordan@wolfpackapps.com')
+        .ariaLabel('Coach to invite')
         // .initialValue('Buddy')
         .targetEvent(ev)
-        .ok('Okay!')
-        .cancel('I\'m a cat person');
+        .ok('Send invite')
+        .cancel('Cancel');
 
       $mdDialog.show(confirm).then(function(result) {
         $log.log('You decided to name your dog ' + result + '.');
