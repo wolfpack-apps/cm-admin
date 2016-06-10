1b5d540e12c5cb07604ce68686a1b5ba2a2fbe22
Jordan Skole <jordan@usebenchmark.com>
2016-06-09 23:56:31 -0400

    use prompt dialog

--- src/app/coaches/ctrl.index.js
+++ src/app/coaches/ctrl.index.js
@@ -6,26 +6,27 @@
     .controller('CoachIndexController', CoachIndexController);
 
   /** @ngInject */
-  function CoachIndexController ($state, $mdSidenav, $mdDialog, Auth, CurrentAuth) {
+  function CoachIndexController ($log, $state, $mdSidenav, $mdDialog, Auth, CurrentAuth) {
 
     var vm = this;
     vm.data = $state.current.data;
 
     vm.inviteCoach = function(ev) {
       // Appending dialog to document.body to cover sidenav in docs app
-      var confirm = $mdDialog.confirm()
+      var confirm = $mdDialog.prompt()
         .title('What would you name your dog?')
-        // .textContent('Bowser is a common name.')
+        .textContent('Bowser is a common name.')
         // .placeholder('Dog name')
         .ariaLabel('Dog name')
         // .initialValue('Buddy')
         .targetEvent(ev)
         .ok('Okay!')
         .cancel('I\'m a cat person');
+
       $mdDialog.show(confirm).then(function(result) {
-        vm.status = 'You decided to name your dog ' + result + '.';
+        $log.log('You decided to name your dog ' + result + '.');
       }, function() {
-        vm.status = 'You didn\'t name your dog.';
+        $log.log('You didn\'t name your dog.');
       });
     };
 
