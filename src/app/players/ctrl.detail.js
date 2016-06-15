9657d34a186336ad3e6363643f4e50129ecced91
Jordan Skole <jordan@usebenchmark.com>
2016-06-14 22:42:14 -0400

    detail view details

--- src/app/players/ctrl.detail.js
+++ src/app/players/ctrl.detail.js
@@ -6,10 +6,30 @@
     .controller('PlayerDetailController', PlayerDetailController);
 
   /** @ngInject */
-  function PlayerDetailController ($state, $mdSidenav, Auth, CurrentAuth) {
+  function PlayerDetailController ($log, $state, $mdSidenav, Auth, CurrentAuth, CurrentCompany, Player) {
 
     var vm = this;
     vm.data = $state.current.data;
+
+    Player
+      .get($state.params.id)
+      .$loaded()
+      .then(function (playerData) {
+        $log.log(playerData)
+        vm.player = playerData;
+      });
+
+    vm.saveCoach = function (route) {
+      vm.player
+        .$save()
+        .then(function (ref) {
+          $state.go(route);
+        })
+    }
+
+    vm.range = function (num) {
+      return new Array(num);
+    }
 
   }
 
